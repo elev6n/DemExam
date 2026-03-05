0486da3b4167ccfd55277c50f89ed3b1a368e860
Владимир Сорокин <vovasorokin148@gmail.com>
2026-03-05 12:18:54 +0300

    add db context

---
+++ src/DemExam.Desktop/Data/AppDbContext.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using DemExam.Desktop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemExam.Desktop.Data;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    public virtual DbSet<UserRole> UserRoles { get; set; }
+
+    public virtual DbSet<UserStatus> UserStatuses { get; set; }
+
+//     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//         => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=dem_exam_database;Username=postgres;Password=admin");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("users_pkey");
+
+            entity.ToTable("users");
+
+            entity.HasIndex(e => e.Login, "users_login_key").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(50)
+                .HasColumnName("first_name");
+            entity.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .HasColumnName("last_name");
+            entity.Property(e => e.Login)
+                .HasMaxLength(20)
+                .HasColumnName("login");
+            entity.Property(e => e.Password)
+                .HasMaxLength(50)
+                .HasColumnName("password");
+            entity.Property(e => e.Patronymic)
+                .HasMaxLength(50)
+                .HasColumnName("patronymic");
+            entity.Property(e => e.UserRole).HasColumnName("user_role");
+            entity.Property(e => e.UserStatus).HasColumnName("user_status");
+
+            entity.HasOne(d => d.UserRoleNavigation).WithMany(p => p.Users)
+                .HasForeignKey(d => d.UserRole)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("users_user_role_fkey");
+
+            entity.HasOne(d => d.UserStatusNavigation).WithMany(p => p.Users)
+                .HasForeignKey(d => d.UserStatus)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("users_user_status_fkey");
+        });
+
+        modelBuilder.Entity<UserRole>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("user_roles_pkey");
+
+            entity.ToTable("user_roles");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Name)
+                .HasMaxLength(20)
+                .HasColumnName("name");
+        });
+
+        modelBuilder.Entity<UserStatus>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("user_statuses_pkey");
+
+            entity.ToTable("user_statuses");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Name)
+                .HasMaxLength(20)
+                .HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ src/DemExam.Desktop/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemExam.Desktop.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public int UserRole { get; set; }
+
+    public int UserStatus { get; set; }
+
+    public string LastName { get; set; } = null!;
+
+    public string FirstName { get; set; } = null!;
+
+    public string? Patronymic { get; set; }
+
+    public string Login { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public virtual UserRole UserRoleNavigation { get; set; } = null!;
+
+    public virtual UserStatus UserStatusNavigation { get; set; } = null!;
+}
