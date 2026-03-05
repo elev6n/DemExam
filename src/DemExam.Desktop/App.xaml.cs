3f122665c26f2d471bdd2e398edcbca6427e1bbb
Владимир Сорокин <vovasorokin148@gmail.com>
2026-03-05 12:27:49 +0300

    add db context to di

--- src/DemExam.Desktop/App.xaml.cs
+++ src/DemExam.Desktop/App.xaml.cs
@@ -1,6 +1,11 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Windows;
+using DemExam.Desktop.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DemExam.Desktop;
 
@@ -9,4 +14,32 @@ namespace DemExam.Desktop;
 /// </summary>
 public partial class App : Application
 {
+    private readonly ServiceProvider _serviceProvider;
+    private readonly IConfiguration _configuration;
+
+    public App()
+    {
+        _configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+        _serviceProvider = services.BuildServiceProvider();
+    }
+
+    private void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton(_configuration);
+        var connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        _serviceProvider?.Dispose();
+        base.OnExit(e);
+    }
 }
