da5dbd18b0f951109c8da127c872d00fbcb9b14e
Владимир Сорокин <vovasorokin148@gmail.com>
2026-03-14 18:51:44 +0300

    add captcha service

---
+++ src/DemExam.Desktop/Services/CaptchaService.cs
@@ -0,0 +1,12 @@
+﻿using DemExam.Desktop.Views;
+
+namespace DemExam.Desktop.Services;
+
+public class CaptchaService : ICaptchaService
+{
+    public bool ShowCaptcha()
+    {
+        var window = new CaptchaWindow();
+        return window.ShowDialog() == true;
+    }
+}
